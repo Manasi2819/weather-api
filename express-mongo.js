61bc0f49e2fb00db2f7d9d7b70d3574237761cbd
Manasi Sadashiv Choudhari <148707047+Manasi2819@users.noreply.github.com>
2024-06-21 20:15:02 +0530

    Update express-mongo.js

--- express-mongo.js
+++ express-mongo.js
@@ -8,9 +8,9 @@ var app = express();
 app.use(bp.json());
 var uid = 1;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Express MongoDB App');
-});
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the Express MongoDB App');
+// });
 
 app.post('/adduser', (req, res) => {
     const user = new empc({ ...req.body });
