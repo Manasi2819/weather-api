ba1e31f5e03bdbff1193de45a79dc9a474a373f2
Manasi Sadashiv Choudhari <148707047+Manasi2819@users.noreply.github.com>
2024-06-22 07:10:13 +0530

    Add files via upload

---
+++ tokenChecker.js
@@ -0,0 +1,18 @@
+const jwt= require('jsonwebtoken')
+const config= require('./config.json')
+
+module.exports=(req,res,next)=>{
+
+    const token= req.headers['x-access-token']
+    if(token){
+        jwt.verify(token,config.secret,function(err,decoded){
+            if(err){
+                return res.status(401).json({"error":true,"message":"unauthrozied access"})
+            }
+            req.decoded= decoded
+            next()
+        })
+    }else {
+        res.status(403).send({"error":true,"message":"no token found"})
+    }
+}
