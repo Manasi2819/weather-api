a12ce5da079b592ba332beaac7db72365c0278bc
Manasi Sadashiv Choudhari <148707047+Manasi2819@users.noreply.github.com>
2024-06-22 07:03:44 +0530

    Delete connectionTest.js

--- connectionTest.js
+++
@@ -1,7 +0,0 @@
-const mongoose= require('mongoose')
-const URL ="mongodb+srv://admin:admin00@cluster0.l8iwhhy.mongodb.net/gfgdb?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(URL)
-
-const user=mongoose.model('demousers',{name:String,email:String})
-const myUser= new user({name:'admin',email:'admin@mail.com'})
-myUser.save().then(()=> console.log('user added'))
